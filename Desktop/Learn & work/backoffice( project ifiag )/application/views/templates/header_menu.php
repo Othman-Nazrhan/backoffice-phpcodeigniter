17897c98e70573caa37c83f3ca2155b6909f7bf1
Othman Na <othman.nazhran@gmail.com>
2020-07-15 16:19:45 +0100

    first commit

---
+++ Desktop/Learn & work/backoffice( project ifiag )/application/views/templates/header_menu.php
@@ -0,0 +1,18 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="#" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>ADN</b></span>
+      <!-- logo f"or regular state and mobile devices -->
+      <span class="logo-lg"> <img src="https://lh3.googleusercontent.com/-ckvuByPy_PA/XXZQmR8Kh5I/AAAAAAAABGU/Xi9_YPbsVssaZbXiRqU8S3eWjT49u5bFQCK8BGAs/s0/2019-09-09.png" alt="" style=" height: 44px;">  </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  
